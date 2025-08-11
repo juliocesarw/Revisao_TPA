32f1cd8408fad3f7433db47df938487f65218c1d
juliocesarw <juliocesarw099@gmail.com>
2025-08-11 08:24:14 -0300

    Inserção concluída

--- readme.md
+++ readme.md
@@ -6,3 +6,9 @@ Faça a opção de buscar por matricula e cpf.
 Quando encontrar, ele deve mostrar os dados do aluno e também dar a opção de remover.
 
 Na inserção não será permitido inserir CPFs nem matriculas iguais, então verifique se o cpf já nao existe antes de inserir outro aluno. A inserção deve ser ordenada por Nome no momento da inserção.
+
+## Nova Tarefa - Tabela hash
+
+<b>Enunciado:</b> Altere o programa da Atividade 1 para agora ter um vetor com 100 elementos da estrutura Alunos. E utilize os dois digitos verificadores do CPF para escolher em qual índice esse Alunos deve ser adicionado. Faça tudo utilizando e medindo métricas de tempo.
+
+Coloque um comentário do tempo gasto para executar. Lembre que os alunos devem ser inseridos de forma ordenada pelo nome e você deve pesquisar se existem CPFs repetidos. Não precisa mais de verificar se a matrícula está repetida.
---
+++ sistema.cpp
@@ -0,0 +1,324 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctype.h>
+#include <cstring>
+#include <ctime>
+
+struct Aluno{
+    char matricula[9];
+    char cpf[15];
+    char nome[40];
+    double nota;
+    int idade;
+    char curso[40];
+    char cidade[40];
+    Aluno *prox;
+    Aluno *ante;
+};
+
+struct Alunos{
+    Aluno *inicio;
+    Aluno *fim;
+    int quantidade;
+};
+
+Alunos a[100];
+
+void inicializa(){
+    for (int i = 0; i < 100; i++)
+    {
+        a[i].inicio = NULL;
+        a[i].fim = NULL;
+        a[i].quantidade = 0;
+    }
+    
+}
+
+//declaracao de funcoes
+Aluno *lerAluno();
+void imprimirAlunos();  
+FILE * abrir_arquivo( const char * arquivo, const char * modo);
+void processoInsercao();
+void menu();
+int escolha();
+bool insere(Aluno *y, int indiceHash);
+Aluno * verificacaoCPF(char * cpf, int indice);
+void pesquisaPorMatricula();
+void pesquisarPorCPF();
+void excluir(Aluno * aluno, int indice);
+int retornaValorCPF(char * cpf);
+
+//============================================================================================
+bool insere(Aluno *y, int indiceHash){
+
+    if(y == NULL) return false;
+    if(a[indiceHash].quantidade == 0){
+        a[indiceHash].inicio = y;
+        a[indiceHash].fim = y;
+        a[indiceHash].quantidade++;
+    }
+    else{
+        Aluno * percorre = a[indiceHash].inicio;
+        while (percorre->prox != NULL && strcmp(percorre->nome, y->nome) < 0)
+        {
+            percorre = percorre->prox;
+        }
+
+        if(verificacaoCPF(y->cpf, indiceHash) == NULL){
+            if(percorre->ante == NULL){
+                //talvez vai ser o primeiro da lista
+                if(percorre->prox == NULL){
+                    //so tinha um elemento na lista
+                    if(strcmp(percorre->nome, y->nome) < 0){
+                        //o elemento a ser inserido deve ser o ultimo
+                        a[indiceHash].inicio->prox = y;
+                        y->ante = a[indiceHash].inicio;
+                        a[indiceHash].fim = y;
+                        a[indiceHash].quantidade++;
+                    }
+                    else{
+                        //o elemento inserido deve ser o primeiro
+                        a[indiceHash].inicio->ante = y;
+                        y->prox = a[indiceHash].inicio;
+                        a[indiceHash].inicio = y;
+                        a[indiceHash].quantidade++;
+                    }
+                }
+                else{
+                    //tinha mais que um, entao o codigo brekou porque ele deveria realmente ser o primeiro
+                    a[indiceHash].inicio->ante = y;
+                    y->prox = a[indiceHash].inicio;
+                    a[indiceHash].inicio = y;
+                    a[indiceHash].quantidade++;
+                }
+            }
+            else if(percorre->prox == NULL){
+                // talvez vai ser o ultimo da lista
+                if(strcmp(percorre->nome, y->nome) < 0){
+                    //realmente vai ser o ultimo da litsa
+                    a[indiceHash].fim->prox = y;
+                    y->ante = a[indiceHash].fim;
+                    a[indiceHash].fim = y;
+                }
+                else{
+                    //vai ser o penultimo
+                    y->ante = a[indiceHash].fim->ante;
+                    a[indiceHash].fim->ante->prox = y;
+                    y->prox = a[indiceHash].fim;
+                    a[indiceHash].fim->ante = y;
+
+                }
+                a[indiceHash].quantidade++;
+            }
+            else{
+                //ta no meio
+                y = percorre->ante;
+                percorre->ante->prox = y;
+                y->prox = percorre;
+                percorre->ante = y;
+                a[indiceHash].quantidade++;
+            }
+        }
+        else{
+            return false;        
+        }
+    }
+
+    return true;
+}
+
+Aluno *lerAluno(){
+    
+    Aluno *b = new Aluno;
+    b->ante = NULL;
+    b->prox = NULL;
+    return b;
+}
+
+void imprimirAlunos(){
+
+    // system("cls");
+
+    for ( int i = 0; i < 100; i++)
+    {
+        Aluno * x = a[i].inicio;
+    
+        while (x != NULL){
+            printf("%s -", x->matricula);
+            printf(" %s -", x->cpf);
+            printf(" %s -", x->nome);
+            printf(" %.2lf -", x->nota);
+            printf(" %d -", x->idade);
+            printf(" %s -", x->curso);
+            printf(" %s", x->cidade);
+            printf("\n");
+            x = x->prox;
+        }
+    }
+    
+
+    system("pause");
+}
+
+FILE * abrir_arquivo(const char * arquivo, const char * modo){
+
+	FILE * arq;
+	arq = fopen(arquivo, modo);
+    if (arq == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        //system("pause");
+    }
+	return arq;
+
+}
+
+void processoInsercao(){
+    FILE * arq;
+    arq = abrir_arquivo("../alunos.csv", "r");
+
+    // int i = 1;
+    
+
+    while(!feof(arq)){
+        Aluno *c = lerAluno();
+        fscanf(arq, "%[^,], %[^,], %[^,] , %lf, %d, %[^,], %[^,\n] ", c->matricula, c->cpf, c->nome, &c->nota, &c->idade, c->curso, c->cidade);
+        int ultimosDigitos = retornaValorCPF(c->cpf);
+        insere(c, ultimosDigitos);
+    }
+    fclose(arq);
+}
+
+int escolha(){
+    
+    int op;
+    //system("cls");
+    printf("\t\tMENU DE PESQUISA\n\n");
+    printf("1 - PESQUISAR POR CPF\n");
+    printf("2 - IMPRIMIR\n");
+    printf("0 - SAIR\n\n");
+    
+    printf("Opcao: ");
+    scanf("%d", &op);
+    return op;
+}
+
+void menu(){
+    int x;
+    do
+    {
+        x = escolha();
+        switch (x)
+        {
+        case 1:
+            pesquisarPorCPF();
+        break;
+        case 2:
+            imprimirAlunos();
+        break;
+        default:
+            break;
+        }
+    } while (x != 0);
+}
+
+Aluno * verificacaoCPF(char * cpf, int indice){
+
+    //system("cls");
+    
+    Aluno * percorre = a[indice].inicio;
+
+    while (percorre != NULL){
+        if(strcmp(percorre->cpf, cpf) == 0){
+            return percorre;
+            break;
+        }
+        percorre = percorre->prox;
+    }
+    
+    return NULL;
+}
+
+void pesquisarPorCPF(){
+    //system("cls");
+    char cpf[15];
+    printf("Digite o CPF: ");
+    scanf("%s", cpf);
+    int i = 0;
+    Aluno * existe = verificacaoCPF(cpf, atoi(1 + strrchr(cpf, '-')) );
+
+    if(existe != NULL){
+        printf("Matricula: %s\n", existe->matricula);
+        printf("CPF: %s\n", existe->cpf);
+        printf("Nome: %s\n", existe->nome);
+        printf("Nota: %.2lf\n", existe->nota);
+        printf("Idade: %d\n", existe->idade);
+        printf("Curso: %s\n", existe->curso);
+        printf("Cidade: %s\n", existe->cidade);
+
+        printf("\nDeseja excluir esse aluno? (S/N): ");
+        char opcao;   
+        scanf(" %c", &opcao);       
+        if(toupper(opcao) == 'S'){  
+            excluir(existe, i);
+            printf("Aluno excluido com sucesso.\n");
+        }
+        else{
+            printf("Exclusao cancelada.\n");
+        }
+    }
+    else{
+        printf("CPF nao encontrado.\n");
+    }
+    system("pause");
+}
+
+void  excluir(Aluno * aluno, int indice){
+    Aluno * aux = aluno;
+    if(aux == a[indice].inicio && aux == a[indice].fim){
+        //so tem um aluno
+        a[indice].inicio = NULL;
+        a[indice].fim = NULL;
+    }
+    else if(aux == a[indice].inicio){
+        //primeiro
+        a[indice].inicio = aux->prox;
+        aux->prox->ante = NULL;
+    }
+    else if(aux == a[indice].fim){
+        //ultimo
+        a[indice].fim = aux->ante;
+        aux->ante->prox = NULL;
+    }
+    else{
+        //meio
+        aux->ante->prox = aux->prox;
+        aux->prox->ante = aux->ante;
+    }
+}
+
+int retornaValorCPF(char * cpf){
+    char var[3];
+    var[0] = cpf[12];
+    var[1] = cpf[13];
+    var[2] = '\0';
+    return atoi(var);
+}
+
+int main(){
+
+    time_t inicio, fim;
+    inicio = clock();
+
+    inicializa();
+    processoInsercao();
+
+    fim = clock();
+    printf("Tempo de leitura: %lf segundos\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
+
+    menu();
+
+    // 2 segundos de inserção 
+
+
+    return 0;
+}
